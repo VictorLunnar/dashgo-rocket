91a9fb6b6f14f9744bb74f6f2e311cd2d6ea2691
unknown <victor@lunnar.team>
2023-01-17 08:36:05 -0300

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import { Flex, Text } from "@chakra-ui/react"
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react"
+import { RiSearchLine, RiNotificationLine, RiUserAddLine } from "react-icons/ri"
 
 export function Header() {
     return (
@@ -10,12 +11,70 @@ export function Header() {
             mx="auto"
             mt="4"
             px="6"
+            align="center"
         >
 
             <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
-                Dashgo
-                <Text as="span" ml="1" color="pink.500">  .</Text>
+                dashgo
+                <Text as="span" ml="1" color="pink.500">.</Text>
             </Text>
+            <Flex
+                as="label"
+                flex="1"
+                py="4"
+                px="8"
+                ml="6"
+                maxWidth={400}
+                alignSelf="center"
+                color="gray.208"
+                position="relative"
+                bg="gray.800"
+                borderRadius="full"
+            >
+                <Input
+                    color="gray.50"
+                    variant="unstyled"
+                    px="4"
+                    mr="4"
+                    placeholder="Buscar na plataforma"
+                    _placeholder={{ color: "gray.400" }}
+
+                >
+
+                </Input>
+                <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+
+
+            <Flex
+                align="center"
+                ml="auto"
+            >
+                <HStack
+
+                    spacing="8"
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.788"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+                <Flex
+                    align="center"
+
+                >
+                    <Box mr="4" textAlign="right">
+                        <Text >Victor Heiermann</Text>
+                        <Text color="gray.300" fontSize="small">victor@lunnar.team</Text>
+                    </Box>
+                    <Avatar size="md" name="Victor Heiermann" src="https://github.com/VictorLunnar.png"/>    
+                </Flex>
+
+            </Flex>
         </Flex>
     )
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,41 @@
+import { Box, Stack, Text,Link, Icon } from "@chakra-ui/react"
+import { RiDashboardLine } from "react-icons/ri"
+export function Sidebar() {
+    return (
+        <Box
+            as="aside"
+            w="64"
+            mr="8"
+        >
+            <Stack
+                spacing="12"
+                align="flex-start"
+
+
+            >
+                <Box>
+                    <Text
+                        fontWeight="bold"
+                        color="gray.400"
+                        fontSize="small"
+                    >
+                        Geral
+                    </Text>
+                    <Stack 
+                    spacing="4"
+                        mt="8"
+                        align="stretch"
+                    >
+                        <Link>
+                            <Icon as={RiDashboardLine}/>
+
+                        </Link>
+                    </Stack>
+                </Box>
+
+            </Stack>
+
+        </Box>
+
+    )
+}
