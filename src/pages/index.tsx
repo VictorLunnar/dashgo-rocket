c34c6859756625a121241e6147ee3b1351c2c16c
unknown <victor@lunnar.team>
2023-01-25 14:17:14 -0300

    Update form funcional

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,32 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import { Input } from "../components/Form/Input";
 import styles from "../styles/Home.module.css";
 // import Input from "../components/Form/input";
+import {useForm} from "react-hook-form"
 import { Flex, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
+import { SubmitHandler } from "react-hook-form/dist/types";
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+type SignInFormData = { 
+  email: string,
+  passowrd: string,
+}
+
+export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm() 
+  const { errors } = formState
+
+  const handleSignIn: SubmitHandler<SignInFormData>= async (values) => {
+    await new Promise(resolve => setTimeout(resolve,2000))
+    console.log(values)
+
+  }
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -19,12 +37,13 @@ export default function Home() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail"></Input>
-          <Input name="password" type="password" label="Senha"></Input>
+          <Input name="email" type="email" label="E-mail" {...register('email ')}></Input>;
+          <Input name="password" type="password" label="Senha" {...register('password')}></Input>
         </Stack>
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button type="submit" mt="6" colorScheme="pink" size="lg" isLoading={formState.isSubmitting}>
           ENTRAR
         </Button>
       </Flex>
